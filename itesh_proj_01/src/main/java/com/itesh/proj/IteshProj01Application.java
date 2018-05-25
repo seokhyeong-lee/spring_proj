9f86ce20dc27a1cc7e64e61f53ad66743868ecf6
seokhyeong-lee <sh.lee6@alticast.com>
2018-05-25 15:55:29 +0900

    Spring Boot Project First Commit...

---
+++ itesh_proj_01/src/main/java/com/itesh/proj/IteshProj01Application.java
@@ -0,0 +1,12 @@
+package com.itesh.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteshProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IteshProj01Application.class, args);
+	}
+}
